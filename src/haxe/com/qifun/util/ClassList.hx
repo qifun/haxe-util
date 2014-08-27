8a0d5897bca3ab5073f74179f710168d0aa54c76
Yang Bo <pop.atry@gmail.com>
2014-08-27 10:25:13 +0800

    增加版权信息

--- src/haxe/com/qifun/util/ClassList.hx
+++ src/haxe/com/qifun/util/ClassList.hx
@@ -1,3 +1,22 @@
+/*
+ * haxe-util
+ * Copyright 2014 深圳岂凡网络有限公司 (Shenzhen QiFun Network Corp., LTD)
+ *
+ * Author: 杨博 (Yang Bo) <pop.atry@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.qifun.util;
 import haxe.macro.Context;
 import haxe.macro.Expr;
@@ -30,13 +49,13 @@ class ClassList
         for (str in classList)
         {
           switch(str)
-          { 
+          {
             case "haxe.io.Output\n":
             case "haxe.io.Input\n":
             case "cs.internal._HxObject.HxObject\n":
             case "java.internal._HxObject.HxObject\n":
             case "haxe.ds.IntMap\n":
-            default: 
+            default:
             {
               f.writeString(str);
               trace(str);
