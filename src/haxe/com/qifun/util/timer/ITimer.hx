4bc0b72210230ae50c2de6e8dddea4e214ac3610
goteet <goteet@126.com>
2014-12-02 17:36:12 +0800

    delete unnecessary interface.

--- src/haxe/com/qifun/util/timer/ITimer.hx
+++ src/haxe/com/qifun/util/timer/ITimer.hx
@@ -5,17 +5,6 @@ import com.qifun.util.ISubscriber;
 @:nativeGen
 interface ITimer
 {
-  
-  /**
-    Tween启动后，经过的时间。只能用于数值计算，不得用于刷新显示。
-  **/
-  public var elapsedMilliseconds(get, never):Int;
-
-  private function get_elapsedMilliseconds():Int;
-  
-  public var durationMilliseconds(get, never):Int;
-
-  private function get_durationMilliseconds():Int;
 
   function run(interruptSubscriber:ISubscriber<Void->Void>, expiredCallback:Void->Void):Void;
 
